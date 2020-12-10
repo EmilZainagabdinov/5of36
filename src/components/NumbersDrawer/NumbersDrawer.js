c462f0806489d96d9f134143c011b753726c2bf5
Emil Zainagabdinov <screw192@gmail.com>
2020-12-10 19:54:36 +0600

    Added css display style

--- src/components/NumbersDrawer/NumbersDrawer.js
+++ src/components/NumbersDrawer/NumbersDrawer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NumbersDrawer = props => {
   return (
-      <div className="FiveOf36">
+      <div className="fiveOf36" style={{display: props.display}}>
         <div className="numberOf36">{props.numbers[0]}</div>
         <div className="numberOf36">{props.numbers[1]}</div>
         <div className="numberOf36">{props.numbers[2]}</div>
