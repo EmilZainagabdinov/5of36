f52c6f0cbee27b7e7a1fcd4cd44ea1a40d1ea363
Emil Zainagabdinov <screw192@gmail.com>
2020-12-10 18:57:51 +0600

    Added elements to be returned by component.

--- src/components/NumbersDrawer/NumbersDrawer.js
+++ src/components/NumbersDrawer/NumbersDrawer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const NumbersDrawer = props => {
   return (
       <div className="FiveOf36">
-
+        <div className="numberOf36">{props.numbers[0]}</div>
+        <div className="numberOf36">{props.numbers[1]}</div>
+        <div className="numberOf36">{props.numbers[2]}</div>
+        <div className="numberOf36">{props.numbers[3]}</div>
+        <div className="numberOf36">{props.numbers[4]}</div>
       </div>
   );
 };
