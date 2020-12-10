9f2015df2cbdf0fb0fd12adfcf1f5845dfd2e29a
Emil Zainagabdinov <screw192@gmail.com>
2020-12-10 18:29:52 +0600

    Imported NumbersDrawer.js component. Added state, numbersGenerator func, and added for rendering.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import NumbersDrawer from "./components/NumbersDrawer/NumbersDrawer";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    numbers: []
+  };
+
+  numbersGenerator = () => {
+    const numbersSet = new Set();
+    const numbersArray = [];
+
+    while (numbersSet.size < 5) {
+      const number = Math.floor(Math.random() * 32 + 5);
+      numbersSet.add(number);
+    }
+
+    for (let item of numbersSet) {
+      numbersArray.push(item);
+    }
+
+    numbersArray.sort((a, b) => a - b);
+
+    this.setState({
+      numbers: numbersArray
+    });
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <button onClick={this.numbersGenerator}>
+            New numbers
+          </button>
+          <NumbersDrawer numbers={this.state.numbers}/>
+        </div>
+    );
+  }
 }
 
 export default App;
